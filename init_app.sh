fdf0f8c06cb409a40f0028a82a5567278bd785fb
Ilya Eliseev <ie@e-ise.com>
2024-12-19 16:59:02 +0300

    init_app

--- init_app.sh
+++ init_app.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -x
 PROJECT="$1"
 mkdir ../htdocs/$PROJECT
-cd ../htdocs/$PROJECT/
-mkdir vendor
-git init .
+cd ../htdocs/
+mkdir common
 git submodule add https://github.com/easyise/eiseIntra vendor/eiseIntra
 git submodule add https://github.com/easyise/eiseXLSX vendor/eiseXLSX
 git submodule add https://github.com/easyise/eiseMail vendor/eiseMail
 git submodule add https://github.com/easyise/eiseREST vendor/eiseREST
 git submodule init
-cp -R vendor/eiseIntra/_system_template/ ./
+cp -R vendor/eiseIntra/_system_template/ ./$PROJECT
 ls -al
-mv common/config_template.php common/config.php
+mv ./$PROJECT/common/config_template.php ./$PROJECT/common/config.php
 cp vendor/eiseIntra/inc_config_template.php vendor/eiseIntra/inc_config.php
